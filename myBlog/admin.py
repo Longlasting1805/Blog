5881469d09e55ceeb35d446e3f6de45c24734510
Akande Al-ameen <longlasting1805@gmail.com>
2022-06-08 10:50:32 +0100

    first commit

--- myBlog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-import blogApi
-from myBlog.models import Blog
-
-admin.site.register(Blog)
-
--- myBlog/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from myBlog.models import Blog
-
-
-class BlogSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Blog
-        fields = ['title', 'author', 'comment']
