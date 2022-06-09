219cb513933f13d21632c6922165ef53cc02de90
Akande Al-ameen <longlasting1805@gmail.com>
2022-06-09 11:55:56 +0100

    date update to blog

--- blogApi/models.py
+++ blogApi/models.py
@@ -12,14 +12,14 @@ class Blog(models.Model):
                               default='')
     comment = models.TextField(max_length=1000,
                                unique=True)
+    date = models.DateTimeField(null=True,
+                                blank=True)
 
     def __str__(self):
         return self.title
 
 
 class Comment(models.Model):
-    date = models.DateTimeField(null=True,
-                                blank=True)
     post = models.ForeignKey(Blog, related_name='BlogPost', on_delete=models.CASCADE)
 
     def __str__(self):
